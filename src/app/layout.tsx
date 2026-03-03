289e0b8ea4234ec1d5fe2eef4a759330e3d1a628
Smith444444 <baoshopfast@gmail.com>
2026-03-03 01:48:18 +0000

    fix: use native async script tag for HB Agency instead of Next.js Script component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -48,9 +47,9 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                <Script
+                <script
                     src="https://d3u598arehftfk.cloudfront.net/prebid_hb_38408_37683.js"
-                    strategy="beforeInteractive"
+                    async
                 />
             </head>
             <body className={`${playfair.variable} ${inter.variable} antialiased`}>
